31f71c3aaa82a5b7018edc71648f4786e738cc28
Simran Nigam <nigamsimran14@gmail.com>
2023-01-28 18:16:11 +0530

    removed unnecessary path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './body/home/home.component';
-import { JitsiComponent } from './body/jitsi/jitsi.component';
-import { ScheduleMeetComponent } from '../app/body/schedule-meet/schedule-meet.component';
 
 const routes: Routes = [
   {path: '', component:HomeComponent},
-  {path: ':id', component: JitsiComponent},
-  {path: 'jitsi', component: JitsiComponent},
-  {path: '', component:HomeComponent},
-  {path: 'scheduleMeet', component:ScheduleMeetComponent},
 ];
 
 @NgModule({
